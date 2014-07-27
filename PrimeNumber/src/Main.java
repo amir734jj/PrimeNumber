28a9014fe05c206379173fc133960ded2aa7b88a
Amir <Amir@Amir-PC>
2014-07-27 14:17:18 -0500

    FINAL

--- PrimeNumber/src/Main.java
+++ PrimeNumber/src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
-	static int DOMAIN = 1000;
+	static int DOMAIN = 10000;
 
 	public static void main(String[] args) {
 		PrimeNumber(DOMAIN);
@@ -11,20 +11,23 @@ public class Main {
 	public static void PrimeNumber(int Domain) {
 
 		ArrayList<Integer> PrimeArray = new ArrayList<Integer>();
-		for (int i = 0; i <= Domain; i++)
+		for (int i = 2; i <= Domain; i++)
 			PrimeArray.add(i);
 		Iterator<Integer> it = PrimeArray.iterator();
 		while (it.hasNext())
 			if (Divisible(it.next()))
 				it.remove();
+		PrimeArray.add(2);
+
 		for (Integer p : PrimeArray)
 			System.out.println("==> " + p);
+
 		System.out.println(PrimeArray.size());
 	}
 
 	public static boolean Divisible(Integer Number) {
 		ArrayList<Integer> IntegerArray = new ArrayList<Integer>();
-		for (int i = 3; i < Number - 1; i++)
+		for (int i = 2; i <= (int) Math.sqrt(Number) + 1; i++)
 			IntegerArray.add(i);
 		Iterator<Integer> it = IntegerArray.iterator();
 		while (it.hasNext())
